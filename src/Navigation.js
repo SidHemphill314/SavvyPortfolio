cde985b1e521eac58e5fc27e5494c00cf4cc3e4f
Sidney Hemphill <hemphillsid@gmail.com>
2018-12-11 21:24:47 -0600

    Links.MapAdded

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,28 +1,24 @@
 import { capitalize } from 'lodash';
 
-function buildLinks(links){
-    var links = '';
-    var list = '';
-    
-    for(let i = 0; i < links.length; i++){
-        if(links[i] !== 'home'){
-            link = links[i];
-        }
-        list += `
-    <li> 
-    <a href="/${links[i]}" data-navigo>${capitalize(links[i])}</a>
-    </li>
-    `;
+function buildLink(link){
+    var href = '';
+
+    if(link !== 'home'){
+        href = link;
     }
 
-    return list;
+    return `
+    <li> 
+    <a href="/${href}" data-navigo>${capitalize(link)}</a>
+    </li>
+    `;
 }
 
 export default function Navigation(state){
     return `
 <div id="navigation">
         <div class="container">
-        ${buildLinks(state[state.active].links)}
+        ${state[state.active].links.map(buildLink).join('')}
         </div>
     </div>
     `;
